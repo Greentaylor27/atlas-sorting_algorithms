95eafc18271f5a4ee3342b23fca334af8b7ab4b8
Greentaylor27 <greentaylor18@gmail.com>
2024-08-08 14:56:41 +0000

    First push of sorting alogrithms

---
+++ bubble_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Function to complete tasks using Bubble sort algorithm
+ * 
+ * @array: A pointer to an array of integers
+ * @size: used to define the size of the array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	int temp;
+	size_t inner, outer;
+
+	for (inner = 0; inner < size - 1; inner++)
+	{
+		for (outer = 0; outer < size - inner -1; outer++)
+		{
+			if (array[outer] > array[outer + 1])
+			{
+				temp = array[outer];
+				array[outer] = array[outer + 1];
+				array[outer + 1] = temp;
+			}
+		}
+	}
+}
