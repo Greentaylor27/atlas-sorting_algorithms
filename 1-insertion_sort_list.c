054487d27dc4c61251367db2548857fe6c5dd3e8
Greentaylor27 <greentaylor18@gmail.com>
2024-08-12 15:23:53 +0000

    another redo of insertion sort I can't figure out what is wrong

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,21 +8,50 @@
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *Variable;
-	listint_t *SortedList = *list;
-	listint_t *temp;
+	listint_t *curr, *index, *nNode;
 
-	if (SortedList == NULL || SortedList->next == NULL)
+	if (!list || !*list || !(*list)->prev)
 	{
-		SortedList = list;
+		return;
 	}
 
-	for (; SortedList->next != NULL; SortedList = SortedList->next)
+	curr = (*list)->next;
+	while (!curr)
 	{
-		Variable = SortedList->prev;
-		if (Variable->n > SortedList->n)
+		nNode = curr->next;
+		index = curr->prev;
+		if (curr->next != NULL)
 		{
-			
+			curr->next->prev = curr->prev;
 		}
+		if (curr->prev != NULL)
+		{
+			curr->prev->next = curr->next;
+		}
+		curr->prev = NULL;
+		curr->next = NULL;
+		while (index != NULL && index->n > curr->n)
+		{
+			index = index->prev;
+		}
+		if (index == NULL)
+		{
+			curr->next = *list;
+			curr->prev = NULL;
+			(*list)->prev = curr;
+			*list = curr;
+		}
+		else
+		{
+			curr->next = index->next;
+			curr->prev = index;
+			if (index->next != NULL)
+			{
+				index->next->prev = curr;
+			}
+			index ->next = curr;
+		}
+		print_list(curr);
+		curr = nNode;
 	}
 }
